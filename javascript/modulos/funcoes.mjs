8ae845a3c533c6e1fa5f9716ea23dd3cd816a6d3
bemibrando <bemi.brando@outlook.com>
2022-05-30 18:46:50 -0300

    JS Module

---
+++ javascript/modulos/funcoes.mjs
@@ -0,0 +1,17 @@
+function mostrarIdade(name, age){
+    return `A idade de ${name} é ${age}`;
+}
+
+function mostrarCidade(name, city){
+    return `A cidade de ${name} é ${city}`;
+}
+
+function mostrarHobby(name, hobby){
+    return `O passa-tempo de ${name} é ${hobby}`;
+}
+
+export{
+    mostrarIdade,
+    mostrarCidade,
+    mostrarHobby
+}
---
+++ javascript/modulos/main.mjs
@@ -0,0 +1,5 @@
+import { mostrarCidade, mostrarHobby, mostrarIdade } from "./funcoes.mjs";
+
+console.log(mostrarIdade('Lala', 12));
+console.log(mostrarCidade('Lulu', 'Choco'));
+console.log(mostrarHobby('Lele', 'Cozinhar'));
